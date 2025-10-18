260c3b9f4e2147c28b4b26c5368995e7ad3c4d05
LuuD04N <doanphongluu082@gmail.com>
2025-10-18 23:20:19 +0700

    feat: avatar and post image with wwwroot

---
+++ backend/Migrations/20251018160021_AddAvatarURLUSER.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAvatarURLUSER : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AvatarUrl",
+                table: "users",
+                type: "varchar(255)",
+                maxLength: 255,
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AvatarUrl",
+                table: "users");
+        }
+    }
+}
